d5161ef16bf2e9e0706850c0f13d6946bcb0850c
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-09-10 19:34:08 +0700

    Add examples

--- lib/Regexp/Pattern/YouTube.pm
+++ lib/Regexp/Pattern/YouTube.pm
@@ -4,7 +4,14 @@ package Regexp::Pattern::YouTube;
 # VERSION
 
 our %RE = (
-    video_id => { pat => qr/[A-Za-z0-9_-]{11}/ },
+    video_id => {
+        pat => qr/[A-Za-z0-9_-]{11}/,
+        examples => [
+            {str=>'aNAtbYSxzuA', gen_args=>{-anchor=>1}, matches=>1},
+            {str=>'aNAtbYSxzuA-', gen_args=>{-anchor=>1}, matches=>0, summary=>'Incorrect length'},
+            {str=>'aNAtb+SxzuA', gen_args=>{-anchor=>1}, matches=>0, summary=>'Contains invalid character'},
+        ],
+    },
 );
 
 1;
